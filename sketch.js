12a0dc72e04918321381b124f287f39dc5beeb74
emmadellavalle <emma.dellavalle@mail.polimi.it>
2025-10-16 21:13:34 +0200

    commit

--- sketch.js
+++ sketch.js
@@ -1,8 +1,30 @@
+let table;
+
 function preload() {
   // put preload code here
+  let table = loadTaste("dataset.csv", "csv", "header");
 }
 
 function setup() {
+  console.log(table);
+
+  let outerPadding = 20;
+  let padding = 10;
+  let itemSize = 30;
+
+  // calcolo il numero di colonne
+  let cols = floor((windowWidth - outerPadding * 2) / (itemSize + padding))
+
+  let row = table.getRowCount() / cols;
+  let totalWeight = outerPadding *2 + rows * itemSize + (rows -1) * padding;
+  console.log("cols:", cols, "rows:", rows);
+
+  //creo il canvas
+  createCanvas(windowWidth, totalHeight);
+  background ("coral");
+  console.log("cols:", cols, "rows:", rows);
+
+  console.log("colonne", cols)
   createCanvas(windowWidth, windowHeight);
   // put setup code here
   const message =
